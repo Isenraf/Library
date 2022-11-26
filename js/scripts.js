4a621d5acbda9f97f0df566d9388f9c577022793
isenraf <bricesilidje@gmail.com>
2022-11-26 16:25:42 +0100

    Use Factory and Module pattern

--- js/scripts.js
+++ js/scripts.js
@@ -1,232 +1,226 @@
-const nodeTableBody = document.querySelector("tbody");
-const nodeTotalBooks = document.querySelector(".total");
+"use strict";
 
-const nodeOpenModal = document.querySelector(".open-modal");
-const nodeModal = document.querySelector(".modal");
-const nodeOverlay = document.querySelector(".overlay");
-const nodeCloseModal = document.querySelector(".close-modal");
+function BookFactory(title, author, pages, read) {
+  const status = read ? "read" : "pending";
 
-const nodeForm = document.querySelector("form");
-const nodeTitle = document.querySelector("#title");
-const nodeAuthor = document.querySelector("#author");
-const nodePages = document.querySelector("#pages");
-const nodeRead = document.querySelector("#read");
-
-// CONSTRUCTOR FUNCTION
-function Book(title, author, pages, read) {
-  if (!(this instanceof Book)) {
-    return new Book(title, author, pages, read);
+  function info() {
+    return `${title} by ${author}, ${pages}, ${read ? "read" : "not read yet"}`;
   }
 
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read ? "read" : "pending";
+  return { title, author, pages, read: status, info };
 }
 
-// STATIC METHOD and ATTRIBUTE
-Book.myLibrary = [
-  {
-    read: "pending",
-    pages: 342,
-    title: "Le guide de l'apprenti millionaire",
-    author: "Christopher Sojtarov",
-  },
-  {
-    read: "pending",
-    pages: 10,
-    title:
-      "Devenez riche: I Will Teach You to Be Rich - Un programme de 6 semaines qui fonctionne vraiment ",
-    author: "Ramit Sethi",
-  },
-  {
-    read: "pending",
-    pages: 812,
-    title: "L'argent, l'art de le maîtriser",
-    author: " Anthony Robbins",
-  },
-  {
-    read: "pending",
-    pages: 191,
-    title: "La chèvre de ma mère",
-    author: "Ricardo Kaniama",
-  },
-  {
-    read: "pending",
-    pages: 291,
-    title:
-      "La psychologie de l'argent: Quelques leçons intemporelles sur la richesse, la cupidité et le bonheur",
-    author: "Morgan Housel",
-  },
-  {
-    read: "pending",
-    pages: 100,
-    title:
-      "Le Quadrant du Cashflow: Un guide pour atteindre la liberté financière",
-    author: "Robert T. Kiyosaki",
-  },
-  {
-    read: "pending",
-    pages: 501,
-    title: "L'autoroute du millionnaire",
-    author: " MJ Demarco",
-  },
-  {
-    read: "pending",
-    pages: 188,
-    title: "L'homme le plus riche de Babylone",
-    author: "Georges Samuel Clason ",
-  },
-  {
-    read: "pending",
-    pages: 336,
-    title: "Père riche, père pauvre",
-    author: " Robert T. Kiyosaki ",
-  },
-  {
-    read: "pending",
-    pages: 254,
-    title: "Réfléchissez et devenez riche",
-    author: "Napoleon Hill",
-  },
-];
+const Library = (function () {
+  let myLibrary = [
+    {
+      read: "pending",
+      pages: 342,
+      title: "Le guide de l'apprenti millionaire",
+      author: "Christopher Sojtarov",
+    },
+    {
+      read: "pending",
+      pages: 10,
+      title:
+        "Devenez riche: I Will Teach You to Be Rich - Un programme de 6 semaines qui fonctionne vraiment ",
+      author: "Ramit Sethi",
+    },
+    {
+      read: "pending",
+      pages: 812,
+      title: "L'argent, l'art de le maîtriser",
+      author: " Anthony Robbins",
+    },
+    {
+      read: "pending",
+      pages: 191,
+      title: "La chèvre de ma mère",
+      author: "Ricardo Kaniama",
+    },
+    {
+      read: "pending",
+      pages: 291,
+      title:
+        "La psychologie de l'argent: Quelques leçons intemporelles sur la richesse, la cupidité et le bonheur",
+      author: "Morgan Housel",
+    },
+    {
+      read: "pending",
+      pages: 100,
+      title:
+        "Le Quadrant du Cashflow: Un guide pour atteindre la liberté financière",
+      author: "Robert T. Kiyosaki",
+    },
+    {
+      read: "pending",
+      pages: 501,
+      title: "L'autoroute du millionnaire",
+      author: " MJ Demarco",
+    },
+    {
+      read: "pending",
+      pages: 188,
+      title: "L'homme le plus riche de Babylone",
+      author: "Georges Samuel Clason ",
+    },
+    {
+      read: "pending",
+      pages: 336,
+      title: "Père riche, père pauvre",
+      author: " Robert T. Kiyosaki ",
+    },
+    {
+      read: "pending",
+      pages: 254,
+      title: "Réfléchissez et devenez riche",
+      author: "Napoleon Hill",
+    },
+  ];
 
-Book.addBookToLibrary = function () {
-  Book.openModal();
-  Book.closeModal();
-  Book.submitBookInfo();
-};
+  function addBookToLibrary(u_title, u_author, u_pages, u_read) {
+    const newBook = BookFactory(u_title, u_author, u_pages, u_read);
+    myLibrary.push(newBook);
+  }
 
-Book.addClickEvent = function () {
-  const nodeListRead = document.querySelectorAll(".read");
-  const nodeListDelete = document.querySelectorAll(".del");
+  function getAllBooks() {
+    return myLibrary;
+  }
 
-  nodeListDelete.forEach((node) => {
-    node.addEventListener("click", function () {
-      const id = +this.dataset.id;
+  function deleteBook(id) {
+    const newLibrary = myLibrary.filter((_, bookId) => bookId !== id);
+    myLibrary = newLibrary;
+  }
 
-      const newArr = Book.myLibrary.filter((_, bookId) => bookId !== id);
-      Book.myLibrary = newArr;
+  function updateBook(id) {
+    const book = myLibrary[id];
+    book.read = book.read === "pending" ? "read" : "pending";
+  }
 
-      Book.printBookFromLibrary();
+  return { getAllBooks, deleteBook, updateBook, addBookToLibrary };
+})();
+
+// UI
+const UI = (function () {
+  const containerBooks = document.querySelector("tbody");
+  const containerTotalBooks = document.querySelector(".total");
+
+  const nodeOpenModal = document.querySelector(".open-modal");
+  const nodeModal = document.querySelector(".modal");
+  const nodeOverlay = document.querySelector(".overlay");
+  const nodeCloseModal = document.querySelector(".close-modal");
+
+  const nodeForm = document.querySelector("form");
+  const nodeTitle = document.querySelector("#title");
+  const nodeAuthor = document.querySelector("#author");
+  const nodePages = document.querySelector("#pages");
+  const nodeRead = document.querySelector("#read");
+
+  function renderBooks() {
+    const markup = Library.getAllBooks()
+      .map((book, id) => {
+        return `
+      <tr>
+          <td>${String(id + 1).padStart(2, "#")}</td>
+          <td>${book.title}</td>
+          <td>${book.author}</td>
+          <td>${book.pages}</td>
+          <td>
+              <div>
+                  <button  data-id="${id}" class="${book.read} status">${
+          book.read
+        }</button>
+              </div>
+          </td>
+          <td>
+              <div>
+                  <button data-id="${id}" class="del">delete</button>
+              </div>
+          </td>
+    </tr>
+      `;
+      })
+      .join("");
+
+    containerBooks.innerHTML = markup;
+  }
+
+  function renderTotalBooks() {
+    containerTotalBooks.textContent = `There are ${
+      Library.getAllBooks().length
+    } total books`;
+  }
+
+  function addClick() {
+    const nodeListDelete = document.querySelectorAll(".del");
+    const nodeListStatus = document.querySelectorAll(".status");
+
+    nodeListDelete.forEach((btnElement) => {
+      btnElement.addEventListener("click", handleDelete.bind(btnElement));
     });
-  });
 
-  nodeListRead.forEach((node) => {
-    node.addEventListener("click", function () {
-      const id = +this.dataset.id;
-      const book = Book.myLibrary[id];
-
-      if (book.read === "pending") {
-        book.read = "read";
-      } else {
-        book.read = "pending";
-      }
-
-      Book.printBookFromLibrary();
+    nodeListStatus.forEach((btnElement) => {
+      btnElement.addEventListener("click", handleStatus.bind(btnElement));
     });
-  });
-};
 
-Book.setTotalBooks = function () {
-  nodeTotalBooks.textContent = Book.myLibrary.length;
-};
+    nodeOverlay.addEventListener("click", handleCloseModal);
 
-Book.printBookFromLibrary = function () {
-  const markup = Book.generateMarkup();
+    nodeOpenModal.addEventListener("click", handleOpenModal);
 
-  Book.clear();
-  Book.setTotalBooks();
+    nodeCloseModal.addEventListener("click", handleCloseModal);
+  }
 
-  nodeTableBody.innerHTML = markup;
+  function addSubmit() {
+    nodeForm.addEventListener("submit", handleSubmitForm);
+  }
 
-  Book.addClickEvent();
-};
+  function clearForm() {
+    nodeAuthor.value = "";
+    nodeTitle.value = "";
+    nodePages.value = "";
+    nodeRead.checked = false;
+  }
 
-Book.clear = function () {
-  nodeTableBody.textContent = "";
-};
+  function handleDelete() {
+    Library.deleteBook(+this.dataset.id);
+    init();
+  }
 
-Book.generateMarkup = function () {
-  return Book.myLibrary
-    .map((book, id) => {
-      return `
-        <tr>
-            <td>${String(id + 1).padStart(2, "#")}</td>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.pages}</td>
-            <td>
-                <div>
-                    <button  data-id="${id}" class="${book.read} read">${
-        book.read
-      }</button>
-                </div>
-            </td>
-            <td>
-                <div>
-                    <button data-id="${id}" class="del">delete</button>
-                </div>
-            </td>
-      </tr>
-        `;
-    })
-    .join("");
-};
+  function handleStatus() {
+    Library.updateBook(+this.dataset.id);
+    init();
+  }
 
-Book.openModal = function () {
-  nodeOpenModal.addEventListener("click", handleOpenModal);
-};
+  function handleOpenModal() {
+    nodeModal.classList.remove("hidden");
+    nodeOverlay.classList.remove("hidden");
+  }
 
-Book.closeModal = function () {
-  nodeCloseModal.addEventListener("click", handleCloseModal);
-  nodeOverlay.addEventListener("click", handleCloseModal);
-};
+  function handleCloseModal() {
+    nodeModal.classList.add("hidden");
+    nodeOverlay.classList.add("hidden");
+  }
 
-Book.submitBookInfo = function () {
-  nodeForm.addEventListener("submit", handleSubmitForm);
-};
+  function handleSubmitForm(e) {
+    e.preventDefault();
 
-Book.clearForm = function () {
-  nodeAuthor.value = "";
-  nodeTitle.value = "";
-  nodePages.value = "";
-  nodeRead.checked = false;
-};
+    const bookAuthor = nodeAuthor.value;
+    const bookTitle = nodeTitle.value;
+    const bookPages = Math.abs(parseInt(nodePages.value));
+    const bookStatus = nodeRead.checked;
 
-// SHARED METHOD'S
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages}, ${
-    this.read ? "read" : "not read yet"
-  }`;
-};
+    Library.addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
+    clearForm();
+    init();
+  }
 
-// HELPER FUNCTIONS || HANDLERS
-function handleOpenModal() {
-  nodeModal.classList.remove("hidden");
-  nodeOverlay.classList.remove("hidden");
-}
+  function init() {
+    renderBooks();
+    renderTotalBooks();
+    addClick();
+    addSubmit();
+  }
 
-function handleCloseModal() {
-  nodeModal.classList.add("hidden");
-  nodeOverlay.classList.add("hidden");
-}
+  return { init };
+})();
 
-function handleSubmitForm(e) {
-  e.preventDefault();
-
-  const bookAuthor = nodeAuthor.value;
-  const bookTitle = nodeTitle.value;
-  const bookPages = Math.abs(parseInt(nodePages.value));
-  const bookStatus = nodeRead.checked;
-
-  Book.myLibrary.push(Book(bookTitle, bookAuthor, bookPages, bookStatus));
-  Book.printBookFromLibrary();
-  Book.clearForm();
-}
-
-// WHEN PAGE LOADS
-window.addEventListener("DOMContentLoaded", function () {
-  Book.printBookFromLibrary();
-  Book.addBookToLibrary();
-});
+UI.init();
